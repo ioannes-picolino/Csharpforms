ba70e9f3828609b8c2f726df2d70d2909885c4ab
Ivan Luiz <124008227+ioannes-picolino@users.noreply.github.com>
2024-01-24 17:58:06 -0300

    Add files via upload
    Resistors forms calculator

---
+++ Leitura_resistores_formulario (2)/Leitura_resistores_formulario/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Leitura_resistores_formulario
+{
+    partial class Leitura_resistores
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_cor1 = new System.Windows.Forms.Label();
+            this.lbl_cor2 = new System.Windows.Forms.Label();
+            this.lbl_cor3 = new System.Windows.Forms.Label();
+            this.lbl_cor4 = new System.Windows.Forms.Label();
+            this.cbx_cor1 = new System.Windows.Forms.ComboBox();
+            this.cbx_cor2 = new System.Windows.Forms.ComboBox();
+            this.cbx_cor3 = new System.Windows.Forms.ComboBox();
+            this.cbx_cor4 = new System.Windows.Forms.ComboBox();
+            this.lbl_titulo = new System.Windows.Forms.Label();
+            this.btn_calcular = new System.Windows.Forms.Button();
+            this.lbl_resultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_cor1
+            // 
+            this.lbl_cor1.AutoSize = true;
+            this.lbl_cor1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_cor1.Location = new System.Drawing.Point(29, 9);
+            this.lbl_cor1.Name = "lbl_cor1";
+            this.lbl_cor1.Size = new System.Drawing.Size(64, 25);
+            this.lbl_cor1.TabIndex = 0;
+            this.lbl_cor1.Text = "Cor 1";
+            // 
+            // lbl_cor2
+            // 
+            this.lbl_cor2.AutoSize = true;
+            this.lbl_cor2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_cor2.Location = new System.Drawing.Point(29, 106);
+            this.lbl_cor2.Name = "lbl_cor2";
+            this.lbl_cor2.Size = new System.Drawing.Size(64, 25);
+            this.lbl_cor2.TabIndex = 1;
+            this.lbl_cor2.Text = "Cor 2";
+            // 
+            // lbl_cor3
+            // 
+            this.lbl_cor3.AutoSize = true;
+            this.lbl_cor3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_cor3.Location = new System.Drawing.Point(29, 226);
+            this.lbl_cor3.Name = "lbl_cor3";
+            this.lbl_cor3.Size = new System.Drawing.Size(64, 25);
+            this.lbl_cor3.TabIndex = 2;
+            this.lbl_cor3.Text = "Cor 3";
+            // 
+            // lbl_cor4
+            // 
+            this.lbl_cor4.AutoSize = true;
+            this.lbl_cor4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_cor4.Location = new System.Drawing.Point(29, 339);
+            this.lbl_cor4.Name = "lbl_cor4";
+            this.lbl_cor4.Size = new System.Drawing.Size(64, 25);
+            this.lbl_cor4.TabIndex = 3;
+            this.lbl_cor4.Text = "Cor 4";
+            // 
+            // cbx_cor1
+            // 
+            this.cbx_cor1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbx_cor1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbx_cor1.FormattingEnabled = true;
+            this.cbx_cor1.Location = new System.Drawing.Point(32, 57);
+            this.cbx_cor1.Name = "cbx_cor1";
+            this.cbx_cor1.Size = new System.Drawing.Size(121, 33);
+            this.cbx_cor1.TabIndex = 4;
+            // 
+            // cbx_cor2
+            // 
+            this.cbx_cor2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbx_cor2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbx_cor2.FormattingEnabled = true;
+            this.cbx_cor2.Location = new System.Drawing.Point(32, 174);
+            this.cbx_cor2.Name = "cbx_cor2";
+            this.cbx_cor2.Size = new System.Drawing.Size(121, 33);
+            this.cbx_cor2.TabIndex = 5;
+            // 
+            // cbx_cor3
+            // 
+            this.cbx_cor3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbx_cor3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbx_cor3.FormattingEnabled = true;
+            this.cbx_cor3.Location = new System.Drawing.Point(32, 288);
+            this.cbx_cor3.Name = "cbx_cor3";
+            this.cbx_cor3.Size = new System.Drawing.Size(121, 33);
+            this.cbx_cor3.TabIndex = 6;
+            // 
+            // cbx_cor4
+            // 
+            this.cbx_cor4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbx_cor4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbx_cor4.FormattingEnabled = true;
+            this.cbx_cor4.Location = new System.Drawing.Point(32, 384);
+            this.cbx_cor4.Name = "cbx_cor4";
+            this.cbx_cor4.Size = new System.Drawing.Size(121, 33);
+            this.cbx_cor4.TabIndex = 7;
+            // 
+            // lbl_titulo
+            // 
+            this.lbl_titulo.AutoSize = true;
+            this.lbl_titulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_titulo.Location = new System.Drawing.Point(334, 9);
+            this.lbl_titulo.Name = "lbl_titulo";
+            this.lbl_titulo.Size = new System.Drawing.Size(229, 25);
+            this.lbl_titulo.TabIndex = 8;
+            this.lbl_titulo.Text = "Leitura de resistores";
+            // 
+            // btn_calcular
+            // 
+            this.btn_calcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_calcular.Location = new System.Drawing.Point(368, 128);
+            this.btn_calcular.Name = "btn_calcular";
+            this.btn_calcular.Size = new System.Drawing.Size(174, 129);
+            this.btn_calcular.TabIndex = 9;
+            this.btn_calcular.Text = "Calcular";
+            this.btn_calcular.UseVisualStyleBackColor = true;
+            this.btn_calcular.Click += new System.EventHandler(this.btn_calcular_Click);
+            // 
+            // lbl_resultado
+            // 
+            this.lbl_resultado.AutoSize = true;
+            this.lbl_resultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_resultado.Location = new System.Drawing.Point(363, 296);
+            this.lbl_resultado.Name = "lbl_resultado";
+            this.lbl_resultado.Size = new System.Drawing.Size(115, 25);
+            this.lbl_resultado.TabIndex = 10;
+            this.lbl_resultado.Text = "Resultado:";
+            // 
+            // Leitura_resistores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl_resultado);
+            this.Controls.Add(this.btn_calcular);
+            this.Controls.Add(this.lbl_titulo);
+            this.Controls.Add(this.cbx_cor4);
+            this.Controls.Add(this.cbx_cor3);
+            this.Controls.Add(this.cbx_cor2);
+            this.Controls.Add(this.cbx_cor1);
+            this.Controls.Add(this.lbl_cor4);
+            this.Controls.Add(this.lbl_cor3);
+            this.Controls.Add(this.lbl_cor2);
+            this.Controls.Add(this.lbl_cor1);
+            this.Name = "Leitura_resistores";
+            this.Text = "Leitura de Resistores";
+            this.Load += new System.EventHandler(this.Leitura_resistores_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_cor1;
+        private System.Windows.Forms.Label lbl_cor2;
+        private System.Windows.Forms.Label lbl_cor3;
+        private System.Windows.Forms.Label lbl_cor4;
+        private System.Windows.Forms.ComboBox cbx_cor1;
+        private System.Windows.Forms.ComboBox cbx_cor2;
+        private System.Windows.Forms.ComboBox cbx_cor3;
+        private System.Windows.Forms.ComboBox cbx_cor4;
+        private System.Windows.Forms.Label lbl_titulo;
+        private System.Windows.Forms.Button btn_calcular;
+        private System.Windows.Forms.Label lbl_resultado;
+    }
+}
+
---
+++ Leitura_resistores_formulario (2)/Leitura_resistores_formulario/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
+
+namespace Leitura_resistores_formulario
+{
+    public partial class Leitura_resistores : Form
+    {
+        public Leitura_resistores()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_calcular_Click(object sender, EventArgs e)
+        {
+            int fx1, fx2, fx3, fx4;
+            fx1 = cbx_cor1.SelectedIndex;
+            fx2 = cbx_cor2.SelectedIndex;
+            fx3 = cbx_cor3.SelectedIndex;
+            fx4 = cbx_cor4.SelectedIndex;
+            lbl_resultado.Text = "Resultado:" + valor_resitor(fx1, fx2, fx3, fx4);
+
+        }
+
+        private void Leitura_resistores_Load(object sender, EventArgs e)
+        {
+            List<string> cores = new List<string>();
+            cores.Add("PRETO");
+            cores.Add("MARROM");
+            cores.Add("VERMELHO");
+            cores.Add("LARANJA");
+            cores.Add("AMARELO");
+            cores.Add("VERDE");
+            cores.Add("AZUL");
+            cores.Add("VIOLETA");
+            cores.Add("CINZA");
+            cores.Add("BRANCO");
+            List<string> cores2 = new List<string>();
+            cores2.Add("DOURADO");
+            cores2.Add("PRATEADO");
+            cbx_cor1.Items.AddRange(cores.ToArray());
+            cbx_cor2.Items.AddRange(cores.ToArray());
+            cbx_cor3.Items.AddRange(cores.ToArray());
+            cbx_cor4.Items.AddRange(cores2.ToArray());
+            cbx_cor1.SelectedIndex= 1;
+            cbx_cor2.SelectedIndex= 0;
+            cbx_cor3.SelectedIndex= 2;
+            cbx_cor4.SelectedIndex= 0;
+        }
+        public string valor_resitor(int fx1, int fx2, int fx3, int fx4)
+        {
+            int resultado = 0;
+            string tolerancia;
+
+            resultado += (fx1) * 10;
+            resultado += (fx2);
+            resultado *= Convert.ToInt32(Math.Pow(10, (fx3)));
+            if (fx4== 0)
+            {
+                tolerancia= "±5%";
+            }
+            else
+            {
+                tolerancia = "±10%";
+            }
+            return string.Format("{0} Ω {1}", resultado, tolerancia);
+        }
+        private string not_eng(double valor)
+        {
+            int[] expoentes = { 12, 9, 6, 3,};
+            string[] simbolos = { "T", "G", "M", "K" };
+
+        }
+    }
+}
